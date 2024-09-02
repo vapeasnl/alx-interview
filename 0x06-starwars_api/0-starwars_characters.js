cec889e8872f0faecc69bd3ec6b55edf6b768604
abdessamadsalim <samimalim1991@gmail.com>
2024-09-02 01:15:39 -0700

    updat 0

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,51 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+
+function getMovieCharacters(movieId) {
+  const filmsUrl = 'https://swapi-api.alx-tools.com/api/films';
+
+  request.get(filmsUrl, (error, response, body) => {
+    if (response.statusCode === 200) {
+      const filmsData = JSON.parse(body);
+      const movieData = filmsData.results.find((film) => film.episode_id.toString() === movieId);
+
+      if (movieData) {
+        const charactersUrls = movieData.characters;
+        const characters = [];
+
+        for (const charUrl of charactersUrls) {
+          request.get(charUrl, (error, response, body) => {
+            if (response.statusCode === 200) {
+              const characterData = JSON.parse(body);
+              characters.push(characterData.name);
+              if (characters.length === charactersUrls.length) {
+                printCharacters(characters, movieData.title);
+              }
+            } else {
+              console.log(`Error: ${response.statusCode}`);
+            }
+          });
+        }
+      } else {
+        console.log(`Movie ID ${movieId} not found.`);
+      }
+    } else {
+      console.log(`Error: ${response.statusCode}`);
+    }
+  });
+}
+
+function printCharacters(characters, movieTitle) {
+  console.log(`Characters in ${movieTitle}:`);
+  for (const character of characters) {
+    console.log(character);
+  }
+}
+
+if (movieId) {
+  getMovieCharacters(movieId);
+} else {
+  console.log('Usage: node 0-starwars_characters.js [Movie ID]');
+}
