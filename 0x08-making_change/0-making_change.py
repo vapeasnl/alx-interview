cc1321b11125009a2245f96dad95de5a60f51908
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-09-17 10:58:55 +0200

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,28 +1,43 @@
 #!/usr/bin/python3
 """
-0x08. Making Change
+Function that returns min num
 """
 
 
 def makeChange(coins, total):
     """
-    Function to determine the fewest number of coins needed to meet a given
-        amount total
+    calculate and return need coins
+    Args:
+        coins list of coins of different values
+        total value to be met
+    Return:
+        Number of coins or -1 if meeting the total is not possible
     """
     if total <= 0:
         return 0
-
-    length = len(coins)
-    optimized = [0 for _ in range(total + 1)]
-
-    for i in range(1, total + 1):
-        smallest = float("inf")
-        for j in range(length):
-            if coins[j] <= i:
-                smallest = min(smallest, optimized[i - coins[j]])
-        optimized[i] = 1 + smallest
-
-    if type(optimized[total]) is not int:
+    if coins == [] or coins is None:
         return -1
+    try:
+        n = coins.index(total)
+        return 1
+    except ValueError:
+        pass
 
-    return optimized[total]
+    coins.sort(reverse=True)
+    coin_count = 0
+    for i in coins:
+        if total % i == 0:
+            coin_count += int(total / i)
+            return coin_count
+        if total - i >= 0:
+            if int(total / i) > 1:
+                coin_count += int(total / i)
+                total = total % i
+            else:
+                coin_count += 1
+                total -= i
+                if total == 0:
+                    break
+    if total > 0:
+        return -1
+    return coin_count
