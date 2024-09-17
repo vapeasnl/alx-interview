ac315986692398eea6b46fb214774fee0bb0d363
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-09-17 10:48:35 +0200

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,21 +1,69 @@
 #!/usr/bin/python3
-"""0-making_change.py"""
+'''Making changes problem'''
+
+
+def get_coins_count(start, coins, total):
+    '''
+    get the number of coins needed to get a total coins needed
+    !! I used this function so that i can test all combination
+    parameters:
+        - start: will be the starting coin denomination
+        - coins: the list of the coins denomination
+        - total: the total coin
+    Return:
+        - number of coins needed to reach total
+        - -1: if we can't reach total
+    '''
+    if total < start:
+        return -1
+
+    origin_total = total
+    total -= start
+    count_coins = 1  # I will use it to count how many coins i used
+    sum_coins = start
+    for x in coins:
+
+        while total - x >= 0 and total >= 0:
+            total -= x
+            count_coins += 1
+            sum_coins += x
+
+    if total != 0 or sum_coins != origin_total:
+        return -1
+
+    return count_coins
 
 
 def makeChange(coins, total):
-    """makeChange func"""
-    if total < 0:
+    '''
+    solving the making change problem
+    parameters:
+        - coins (list[int]): a list of coins denominations
+        - total (int): the total amount of coins
+    Return:
+        - the number of coins to get the given total coins
+        - 0: if total <= 0
+        - -1: if we can't reach the total coins
+    '''
+    if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    coins.sort(reverse=True)
+    list_of_counts = []
 
-    if total > 1278650:
-        for i in range(100000000):
-            pass
+    i = 0
+    for start in coins:
 
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+        coins_count = get_coins_count(start, coins[i:], total)
+        list_of_counts.append(coins_count)
+        i += 1
 
-    return dp[total] if dp[total] != float('inf') else -1
+    if max(list_of_counts) == -1:
+        return -1
+
+    # removing all -1 if there are any
+    list_of_counts = [c for c in list_of_counts if c != -1]
+
+    list_of_counts.sort()
+
+    return list_of_counts[0]
