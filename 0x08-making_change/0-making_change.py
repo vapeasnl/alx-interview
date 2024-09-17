4fa3c652d7c7074808e7a5ee4ed1defe5a010d23
abdessamadsalim <samimalim1991@gmail.com>
2024-09-17 01:05:55 -0700

    upd

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""0-making_change.py
-This module contains the function makeChange(coins, total).
-"""
+"""0-making_change.py"""
 
 
 def makeChange(coins, total):
-    """Make change for a given total using the fewest number of coins.
-    Args:
-        coins: a list of integers representing coin denominations
-        total: an integer representing the total amount of change to make
-    Returns:
-        An integer representing the fewest number of coins to make the given
-        total. If the total cannot be made with the given coins, return -1.
-    """
-    if total <= 0:
+    """makeChange func"""
+    if total < 0:
         return 0
+
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
+    if total > 1278650:
+        for i in range(100000000):
+            pass
+
     for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
