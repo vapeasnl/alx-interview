b1319ff773c445b619158fbac56eb1682a8ae1b0
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-09-17 10:56:12 +0200

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
-""" Change comes from within """
+"""
+0x19. Making Change
+"""
 
 
 def makeChange(coins, total):
-    """ return the fewest number of coins needed to meet givern amount """
+    """
+    Function to determine the fewest number of coins needed to meet a given
+        amount total
+    """
     if total <= 0:
         return 0
-    dp = [total + 1] * (total + 1)
-    dp[0] = 0
+
+    length = len(coins)
+    optimized = [0 for _ in range(total + 1)]
+
     for i in range(1, total + 1):
-        for coin in coins:
-            if i - coin >= 0:
-                dp[i] = min(dp[i], 1 + dp[i - coin])
-    return dp[-1] if dp[-1] != total + 1 else -1
+        smallest = float("inf")
+        for j in range(length):
+            if coins[j] <= i:
+                smallest = min(smallest, optimized[i - coins[j]])
+        optimized[i] = 1 + smallest
+
+    if type(optimized[total]) is not int:
+        return -1
+
+    return optimized[total]
